ef9ec1d0bdb6a7629064e891d4c5a54935b02514
Md Suhail Arif <suhailarif14@gmail.com>
2022-07-02 21:19:27 +0530

    js update

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+const mobi_nav = document.querySelector(".mob-nav")
+const head = document.querySelector(".header")
+
+
+const toggleNavbar = () =>{
+    head.classList.toggle("active")
+}
+
+    mobi_nav.addEventListener('click', () => toggleNavbar());
